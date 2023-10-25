fa8863ed656c08ce1b410fb497e1273d9313c716
vertheragna133 <andresgonz133@gmail.com>
2023-10-24 23:45:29 -0400

    Final

---
+++ codigo.php
@@ -0,0 +1,21 @@
+<?php
+class convertir {
+public $tasa = 34.88;
+	function convertirdolar($montobs) {
+		return $montobs / $this->tasa;
+	}
+}
+
+class Operaciones extends convertir {
+    public function sumar($cantidad) {
+        return $cantidad + 100;
+    }
+}
+
+class Saludo {
+public $hola = 'Mi nombre es Andres Gonzalez, De T2-INF-1 Sección 1 informatica trayecto 2';
+	public function buenas() {
+		return $this->hola;
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+    <?php
+    require "codigo.php";
+    $resolver = new Operaciones;
+	$saludo = new Saludo;
+        echo $resolver->convertirdolar(75);
+        echo "<br>";
+        echo $resolver->sumar(75);
+        echo "<br>";
+		echo $saludo->buenas();
+    ?>
+    </body>
+<html>
